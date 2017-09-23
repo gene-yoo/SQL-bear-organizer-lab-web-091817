b9f6365e3814a3847ea55577e11d666959448a4b
gene-yoo <gene.y.yoo@gmail.com>
2017-09-23 17:34:13 -0400

    Lab complete.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+  ("Mr. Chocolate", 1, "M", "Red", "friendly", 1),
+  ("Rowdy", 2, "M", "Orange", "friendly", 1),
+  ("Tabitha", 1, "F", "Yellow", "friendly", 1),
+  ("Sergeant Brown", 1, "F", "Green", "friendly", 1),
+  ("Melissa", 1, "M", "Blue", "friendly", 1),
+  ("Grinch", 1, "F", "Indigo", "friendly", 1),
+  ("Wendy", 1, "M", "Violet", "friendly", 1),
+  (NULL, 1, "F", "Red", "friendly", 1);
